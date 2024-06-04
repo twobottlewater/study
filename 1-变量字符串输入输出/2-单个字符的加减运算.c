cb76f4b5c82e534fefe1b412e40e7875dfdf396d
twobottlewater <128497359+twobottlewater@users.noreply.github.com>
2024-06-04 14:40:15 +0800

    Delete 1-变量字符串输入输出 directory
    正常改动

--- 1-变量字符串输入输出/2-单个字符的加减运算.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-	int x='c';
-	
-	printf("%c\n",'a'+(x-'a'+1));
-	return 0;
-}
--- 1-变量字符串输入输出/7-exit函数也可以刷新缓冲区.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-
-int main()
-{
-	printf("各位同学你等会看不到这句话!");	
-	exit(0);  //结束程序的时候，会帮你刷新缓冲区
-}
