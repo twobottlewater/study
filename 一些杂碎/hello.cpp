214ff42c8402e52378eecb20dc2e9eb56eb0942e
parkin985 <yutao644@outlook.com>
2024-07-13 14:58:02 +0800

    56

--- 一些杂碎/hello.cpp
+++ 一些杂碎/hello.cpp
@@ -1,6 +1,40 @@
-//写一个hello的cppp
 #include <iostream>
-int main(){
-    std::cout << "hello world" << std::endl;
+
+using namespace std;
+
+class Doublex
+{
+public:
+    int Double(int x)
+    {
+        return  2*x;
+    }
+    long Double(long x)
+    {
+        return  2*x;
+    }
+
+    float Double(float x)
+    {
+        return  2*x;
+    }
+
+    double Double(double x)
+    {
+        return  2*x;
+    }
+
+
+
+};
+
+int main()
+{
+    Doublex dou;
+    float n=2.56;
+
+    n=dou.Double(n);
+    cout<<"输出"<<n<<endl;
+
     return 0;
 }
